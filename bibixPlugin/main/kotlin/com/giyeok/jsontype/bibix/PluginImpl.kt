90c71c37dd0a38e9e36e3c7f8617a0c7b857ab35
Joonsoo Jeon <giyeok7@gmail.com>
2024-02-10 21:12:57 +0900

    Package

--- bibixPlugin/main/kotlin/com/giyeok/jsontype/bibix/PluginImpl.kt
+++ bibixPlugin/main/kotlin/com/giyeok/jsontype/bibix/PluginImpl.kt
@@ -7,16 +7,25 @@ import com.giyeok.jsontype.JsonClassCodeGen
 import com.giyeok.jsontype.JsonTypeCompiler
 import com.giyeok.jsontype.JsonTypeParser
 import com.giyeok.jsontype.util.KotlinCodeWriter
+import kotlin.io.path.createDirectories
 import kotlin.io.path.readText
 import kotlin.io.path.writeText
 
 class PluginImpl {
   fun build(ctx: BuildContext): BibixValue {
     val parsed = JsonTypeParser.parse(ctx.getFileField("schema").readText())
+
+    val destDir = if (parsed.pkg != null) {
+      parsed.pkg!!.name.names.fold(ctx.clearDestDirectory()) { m, i -> m.resolve(i.name) }
+    } else {
+      ctx.clearDestDirectory()
+    }
+    destDir.createDirectories()
+
     val compiler = JsonTypeCompiler(parsed.pkg?.name)
     compiler.compile(parsed.defs)
 
-    val writer = KotlinCodeWriter(compiler.pkg?.names?.joinToString("."))
+    val writer = KotlinCodeWriter(compiler.pkg?.names?.joinToString(".") { it.name })
 
     compiler.classes.forEach { (_, cls) ->
       writer.writeLine()
@@ -36,7 +45,7 @@ class PluginImpl {
       }
     }
 
-    val destFile = ctx.clearDestDirectory().resolve(ctx.getStringArg("fileName"))
+    val destFile = destDir.resolve(ctx.getStringArg("fileName"))
     destFile.writeText(writer.toString())
 
     return FileValue(destFile)
